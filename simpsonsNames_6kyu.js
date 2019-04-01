388019d271104e8a5169bc9127fc8a7708cf372f
smd9788 <smd09876@gmail.com>
2019-04-01 15:40:07 -0400

    cleaned up erroneous comments.

--- simpsonsNames_6kyu.js
+++ simpsonsNames_6kyu.js
@@ -1,23 +1,23 @@
-// 6kyu
-// Format a string of names like 'Bart, Lisa & Maggie'.
-//
+// 6kyu: Format a string of names like 'Bart, Lisa & Maggie'.
+
 // Given: an array containing hashes of names
-//
+
 // Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
-//
+
 // Example:
 // list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
 // returns 'Bart, Lisa & Maggie'
-//
+
 // list([ {name: 'Bart'}, {name: 'Lisa'} ])
 // returns 'Bart & Lisa'
-//
+
 // list([ {name: 'Bart'} ])
 // returns 'Bart'
-//
+
 // list([])
 // returns ''
 
+// my solution:
 function list(names){
   const getNameValues = names.map(function(object) {
     const eachName = object.name
--- triangle_7kyu.js
+++ triangle_7kyu.js
@@ -1,9 +1,10 @@
-// 7kyu
-// Is this a triangle? (JavaScript)
-//
+// 7kyu: Is this a triangle? (JavaScript)
+
 // Description:
-// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
-//
+// Implement a method that accepts 3 integer values a, b, c. The method should
+// return true if a triangle can be built with the sides of given length and false
+// in any other case.
+
 // (In this case, all triangles must have surface greater than 0 to be accepted).
 
 function isTriangle(a,b,c) {
